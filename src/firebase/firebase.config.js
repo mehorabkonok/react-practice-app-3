115f420049112e716cce98c51468ab20f17da0c5
Konok Mehorab Shibli <mehorabkonok@gmail.com>
2024-08-28 19:57:23 +0600

    config file changed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyBnNaBdXwnyy3dKNfsEB78JawDdpQDGbJU",
+  authDomain: "react-practice-app-3.firebaseapp.com",
+  projectId: "react-practice-app-3",
+  storageBucket: "react-practice-app-3.appspot.com",
+  messagingSenderId: "394631314162",
+  appId: "1:394631314162:web:63f93b6d56a6f46402ae6b",
 };
 
 // Initialize Firebase
